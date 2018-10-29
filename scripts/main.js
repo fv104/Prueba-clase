3e519a91c04b04c9f8a2d7c0552256f626df778a
Felix Vicenta <fv104@hotmail.com>
2018-10-29 14:13:46 +0100

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,61 @@
+
+console.log(1);
+
+console.log('1');
+
+console.log(true);
+
+//var miVariable;
+//console.log(miVariable);
+
+/*
+console.log({
+    name: 'Felix',
+    bool: true,
+    defined: undefined,
+    miVariable: miVariable
+});
+
+
+var miObjeto = {
+    'name': 'Felix',
+    bool: true
+};
+
+console.log(miObjeto);
+console.log(miObjeto.name);
+console.log(miObjeto['bool']);
+*/
+/*
+var arr = new Array(1,2,3,'Hola');
+var arr2 = [1,2,3,'Hola'];
+
+console.log('Array indice 3 ->',arr[3]);
+
+var miVar = (1+1);
+
+
+//var variable4 = prompt('Introduce un valor');
+
+//console.log(parseInt(variable4));
+//console.log(arr2);
+console.log(9/6);
+console.log(9%6);
+
+var miVar=2;
+console.log(++miVar);
+console.log(miVar++);
+console.log(miVar);
+console.log(new Date(0));
+
+console.log(false && true); //false
+console.log(false && false);//false
+console.log(true && true);
+console.log(true && false);
+*/
+console.log(!!'a');
+
+var asignar = true;
+
+var miVariable = asignar && 'Funciona';
+console.log(miVariable);
